bbe8840cb30f92a1fe66052a6659c404d69baeee
Cody Njos <cody.njos@live.com>
2021-04-27 18:31:29 -0500

    added search bar that updates redux store on change

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,15 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { TextField } from "@material-ui/core"
 function SearchPage() {
     const dispatch = useDispatch();
     const reduxStore = useSelector(store => store);
-
+    const [search, setSearch] = useState("")
     useEffect(() => {
-        dispatch({ type: 'FETCH_SEARCH', payload: "asdf" });
+     console.log('search page')
     }, []);
+
+   
+    useEffect(() => {
+        if(search != ""){
+        dispatch({type: "FETCH_SEARCH", payload: search})
+        }
+    }, [search]);
+    
     return(
         <>
         <h1>search</h1>
+        <TextField
+        label = "search"
+        value={search}
+        onChange={(e)=> setSearch(e.target.value)}
+        />
         </>
     )
 }
