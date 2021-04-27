02a0d7e694b59a1b9c91a74b2c8a88119ef96bea
Cody Njos <cody.njos@live.com>
2021-04-27 16:50:50 -0500

    set up giphy route

---
+++ server/routes/giphy.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const { default: axios } = require('axios');
+const router = express.Router();
+
+router.get('/search/:search', (req, res) => {
+    const GIPHY_URL = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${req.params.search}&limit=10`
+    axios.get(GIPHY_URL).then(response => {
+        res.status(200).send(response.data);
+    }).catch(err => {
+        console.log('Error getting gif', err.response)
+        res.send(500);
+    });
+  });
+
+  module.exports = router
--- server/server.js
+++ server/server.js
@@ -6,12 +6,19 @@ const app = express();
 // App PORT set with production check
 const PORT = process.env.PORT || 5000;
 
+// Route includes
+const gifRouter = require('./routes/giphy.router');
+
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files
 app.use(express.static('build'));
+
+// Routes
+app.use('/api/gif', gifRouter);
+
 
 // Listen
 app.listen(PORT, () => {
