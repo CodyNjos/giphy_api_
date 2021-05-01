028e617dc05862d417cdfa261842eaf2a092024f
Cody Njos <cody.njos@live.com>
2021-05-01 13:46:16 -0500

    added hash router to app.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,32 @@
+import React, { useEffect } from 'react';
+import {HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+import { useDispatch } from 'react-redux';
+
 import SearchPage from '../SearchPage/SearchPage'
 import './App.css'
 import Login from '../Login/Login'
 
 function App() {
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_USER' });
+  }, [dispatch]);
   return (
+    <Router>
     <div className='app'>
+    <ProtectedRoute
+      path="/search">
     <SearchPage/>
+    </ProtectedRoute>
+
+    <Route
+      path="/login">
     <Login/>
+    </Route>
     </div>
+    </Router>
   )
 
 };
