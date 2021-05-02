1c129b8149fc72f3c5c9dab4f1d5d401158c4aa8
Cody Njos <cody.njos@live.com>
2021-05-02 11:52:39 -0500

    added route to select all rated gifs by user id

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -3,15 +3,31 @@ const pool = require('../modules/pool');
 const { default: axios } = require('axios');
 const router = express.Router();
 
+
+// Request used to search gifs through the giphy API
 router.get('/search/:search', (req, res) => {
- 
     const GIPHY_URL = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${req.params.search}&limit=50`
     axios.get(GIPHY_URL).then(response => {
         res.status(200).send(response.data);
     }).catch(err => {
-        console.log('Error getting gif', err.response)
+        console.log('Error searching gifs', err.response)
         res.send(500);
     });
   });
 
+// Request used to get rated gifs
+router.get('/rated', (req, res) => {
+    const query = 
+    `SELECT * 
+    FROM "rated"
+    WHERE "user_id" = $1`
+    pool.query(query, [req.body.id])
+        .then(result => {
+            console.log(result.rows)
+            res.send(result.rows)
+    }).catch(err => {
+        console.log('Error getting rated gifs', err.response)
+        res.send(500);
+    });
+  });
   module.exports = router
