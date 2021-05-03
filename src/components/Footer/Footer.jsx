c382f50046a3f96d614e40f6d4f31d527f49d359
Cody Njos <cody.njos@live.com>
2021-05-03 02:51:32 -0500

    Footer link opens new tab rather than redirecting in current tab

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import "./Footer.css"
 function Footer () {
     return(
         <div className = "footer" >
-            <a href="https://www.linkedin.com/in/njos/"> &copy; Cody Njos, {new Date().getFullYear()} </a>
+            <a href="https://www.linkedin.com/in/njos/" target="_blank" rel="noreferrer"> &copy; Cody Njos, {new Date().getFullYear()} </a>
         </div>
     )
 }
